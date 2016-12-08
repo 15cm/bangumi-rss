5e57a73aa21f6cdf19e9dd0768f5616f669d87ef
15cm <sinkerine@gmail.com>
2016-12-09 00:32:44 +0800

    [!] Change script name show in "help"

--- rss/bangumi-rss.js
+++ rss/bangumi-rss.js
@@ -7,14 +7,15 @@ const bgm = require('./bgm.js')
 const Aria2 = require('aria2')
 const schedule = require('node-schedule')
 const _ = require('lodash')
+const NAME = path.basename(__filename, '.js')
 const argv = require('yargs')
-      .usage('Usage: $0 <command> [args]')
+      .usage(`Usage: ${NAME} <command> [args]`)
       .boolean('nd')
       .describe('nd', "Not download. '--nd' means not send new feeds to Aria2")
       .command('add <name> <rss>', 'Add a bangumi with rss url', {}, argv => bgm.add(argv.name, argv.rss))
-      .example('$0 add oc9 \'https://www.nyaa.se/?page=rss&term=Horriblesubs+Occultic;Nine+(1080p)\'')
+      .example(`${NAME} add oc9 'https://www.nyaa.se/?page=rss&term=Horriblesubs+Occultic;Nine+(1080p)'`)
       .command('remove <name>', 'Remove a bangumi', {}, argv => bgm.remove(argv.name))
-      .example('$0 remove oc9')
+      .example(`${NAME} remove oc9`)
       .command('check [name] [option]', 'Check new feeds for bangumi[s]', {}, argv => {
         var isDownload = !argv.nd
         if(argv.name) {
@@ -23,7 +24,7 @@ const argv = require('yargs')
           checkFeedAll(isDownload)
         }
       })
-      .example('$0 check oc9')
+      .example(`${NAME} check oc9`)
       .command('list [name]', 'List infos of bangumi[s]', {}, argv => {
         if(argv.name) {
           bgm.list(argv.name) 
@@ -31,7 +32,7 @@ const argv = require('yargs')
           bgm.listAll()
         }
       })
-      .example('$0 list oc9')
+      .example(`${NAME} list oc9`)
       .command('schedule [cron] [option]',
                'Schedule job of checking all new feeds with cron format(Default: check every 30 min)',
                {
@@ -46,7 +47,7 @@ const argv = require('yargs')
                    checkFeedAll(isDownload)
                  })
                })
-      .example('$0 schedule')
+      .example(`${NAME} schedule`)
       .epilog('copyright 15cm')
       .demand(1)
       .help()
@@ -66,7 +67,7 @@ function sendToAria2(feeds) {
   })
 }
 
-function checkFeed(bgnName, isDownload = true) {
+function checkFeed(bgmName, isDownload = true) {
   bgm.check(bgmName).then(feeds => {
     if(isDownload) sendToAria2(feeds)
   }).catch(err => {
